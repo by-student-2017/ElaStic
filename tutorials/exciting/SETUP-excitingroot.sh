698861ded9ef4f20740189164f3bfc5a322caf27
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-08-18 13:00:54 +0900

    Delete tutorials/exciting/SETUP-excitingroot.sh

--- tutorials/exciting/SETUP-excitingroot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#-------------------------------------------------------------------------------
-IXML=input.xml
-TMP=$1
-len=${#TMP}
-if [ "$len" -gt 0 ] ; then IXML=$TMP ; fi
-awk -v xcr=$EXCITINGROOT '{ gsub(/\$EXCITINGROOT/, xcr); print }' $IXML > tmpfile
-mv tmpfile $IXML
-
