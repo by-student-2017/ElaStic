e79b984b269682a373616d5c74489128ece01488
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-08-18 18:50:25 +0900

    Add files via upload

--- ElaStic_WIEN2k_init.sh
+++ ElaStic_WIEN2k_init.sh
@@ -13,16 +13,46 @@
 # EXPLANATION:
 # 
 #__________________________________________________________________________________________________
-dpath=`pwd`
-dirnm=`basename $dpath`
-for dir_num in `ls -d *_??`; do
-    cd $dir_num
-    #******************
-    #x sgroup
-    #cp -f $dir_num.struct_sgroup $dir_num.struct
-    WIEN2k_init_lapw_silent -b -vxc 13  -ecut -10.0  -in1_rkmax 8.0  -in2_method GAUSS  -in2_gmax 14  -in2_smear 0.01  -mix 0.1  -kgen_numk 2500  -kgen_shift 0  -inM_method PORT  -inM_tolf 0.1
-    #cp -f ../$dirnm.in0 $dir_num.in0
-    #******************
-    cd ..
+#dpath=`pwd`
+#dirnm=`basename $dpath`
+#for dir_num in `ls -d *_??`; do
+#    cd $dir_num
+#    #******************
+#    #x sgroup
+#    #cp -f $dir_num.struct_sgroup $dir_num.struct
+#    WIEN2k_init_lapw_silent -b -vxc 13  -ecut -10.0  -in1_rkmax 8.0  -in2_method GAUSS  -in2_gmax 14  -in2_smear 0.01  -mix 0.1  -kgen_numk 2500  -kgen_shift 0  -inM_method PORT  -inM_tolf 0.1
+#    #cp -f ../$dirnm.in0 $dir_num.in0
+#    #******************
+#    cd ..
+#done
+#exit
+
+# Set number of threads and CPUs
+#export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=$(($(nproc) / 2))
+#NCPUs=$(($(nproc) / 2))
+
+label=`ls -d Dst??`
+for Dstn in $label ; do
+    cd $Dstn
+    Dstn_num_list=`ls -d ${Dstn}_??`
+    for Dstn_num in $Dstn_num_list ; do
+        cd $Dstn_num/
+        cp -f $Dstn_num.xml input.xml
+        echo
+        echo '        +--------------------------------------+'
+        echo '        | SCF calculation of "'${Dstn_num}'" starts |'
+        echo '        +--------------------------------------+'
+        #******************
+        #x sgroup
+        #cp -f $dir_num.struct_sgroup $dir_num.struct
+        csh WIEN2k_init_lapw_silent.csh -b -vxc 13  -ecut -10.0  -in1_rkmax 8.0  -in2_method GAUSS  -in2_gmax 14  -in2_smear 0.01  -mix 0.1  -kgen_numk 2500  -kgen_shift 0  -inM_method PORT  -inM_tolf 0.1
+        #cp -f ../$dirnm.in0 $dir_num.in0
+        #******************
+        time runsp_lapw -I -fc 1.0 -i 100
+        #time runsp_lapw -I -fc 1.0 -i 100
+        date
+        cd ../
+    done
+    cd ../
 done
-exit
