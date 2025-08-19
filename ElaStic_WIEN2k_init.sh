6d0e13e49ac5cdb9c59e77c43e7aa614cbbc5c02
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-08-19 09:25:24 +0900

    Add files via upload

--- ElaStic_WIEN2k_init.sh
+++ ElaStic_WIEN2k_init.sh
@@ -25,7 +25,7 @@ for Dstn in $label ; do
         echo '        +--------------------------------------+'
         echo '        | SCF calculation of "'${Dstn_num}'" starts |'
         echo '        +--------------------------------------+'
-        WIEN2k_init_lapw_silent.csh -b -red 7.0 -vxc 13  -ecut -10.0  -in1_rkmax 8.0  -in2_method GAUSS  -in2_gmax 14  -in2_smear 0.01  -mix 0.1  -kgen_numk 2500  -kgen_shift 0  -inM_method PORT  -inM_tolf 0.1
+        WIEN2k_init_lapw_silent.csh -b -red 8.0 -vxc 13  -ecut -10.0  -in1_rkmax 8.0  -in2_method GAUSS  -in2_gmax 14  -in2_smear 0.01  -mix 0.1  -kgen_numk 2500  -kgen_shift 0  -inM_method PORT  -inM_tolf 0.1
         time run_lapw -NI -i 200
         #time runsp_lapw -NI -i 200
         date
